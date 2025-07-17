279db45c3ab188c472e69343e7a1f61617f92b54
kaewin <kaelyn_parris@protonmail.com>
2025-07-17 12:26:18 -0500

    Revert "Merge branch 'develop' into mpi_windows"
    This reverts commit a2f9ba22eb0992dc8be0bb52a911051b14dca114, reversing
    changes made to ffbc6a56cd008c3dd36b28e087eba8c40c8c13ea.
    
    Rolling back due to compilation error.

--- src/KokkosComm/KokkosComm.hpp
+++ src/KokkosComm/KokkosComm.hpp
@@ -36,9 +36,7 @@
 #include "mpi/recv.hpp"
 #include "mpi/send.hpp"
 
-#include "mpi/broadcast.hpp"
 #include "mpi/allgather.hpp"
-#include "mpi/allreduce.hpp"
 #include "mpi/alltoall.hpp"
 #include "mpi/reduce.hpp"
 
