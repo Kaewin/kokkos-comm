7bd0c260b46f59c66e7569f90584d88c04e61e8d
kaewin <kaelyn_parris@protonmail.com>
2025-07-22 16:43:08 -0500

    Removed references to channel

--- src/KokkosComm/KokkosComm.hpp
+++ src/KokkosComm/KokkosComm.hpp
@@ -25,7 +25,7 @@
 #if defined(KOKKOSCOMM_ENABLE_MPI)
 #include "mpi/mpi_space.hpp"
 
-#include "mpi/channel.hpp"
+//#include "mpi/channel.hpp"
 #include "mpi/window.hpp"
 #include "mpi/comm_mode.hpp"
 #include "mpi/handle.hpp"
