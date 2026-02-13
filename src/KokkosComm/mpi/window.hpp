1a6646e24ac17a55ef78f6511529d3b246bdb8ec
Kaewin <gerbald@protonmail.com>
2026-02-13 12:21:50 -0600

    Added in Accumulate.  Cleaning up code.

--- src/KokkosComm/mpi/window.hpp
+++ src/KokkosComm/mpi/window.hpp
@@ -39,46 +39,35 @@ class Window {
 
   ~Window() { MPI_Win_free(&win); }
 
-  void fence (int assert = 0) { MPI_Win_fence(assert, win); }
-
-
-  // TODO: ADD Accumulate
-
-  
-  /*
-  Function: put
-  -----------------
-  Description:
-    Performs an MPI Put operation to write data from the origin address
-    to the target rank's memory at the specified displacement.
-  */
+  // Data Transfer Functions:
+  // Put
   template <typename T>
   void put(const T* origin_addr, int count, int target_rank, MPI_Aint target_disp) {
-    MPI_Datatype datatype = get_mpi_datatype<T>();
-
+    MPI_Datatype datatype = KokkosComm::Impl::mpi_type_v<T>;
     MPI_Put(origin_addr, count, datatype, target_rank, target_disp, count, datatype, win);
   }
-
-  /*
-  Function: get
-  -----------------
-  Description:
-    Performs an MPI Get operation to read data from the source rank's memory
-    at the specified displacement into the origin address.
-  */
+  // Get
   template <typename T>
   void get(T* origin_addr, int count, int source_rank, MPI_Aint source_disp) {
-    MPI_Datatype datatype = get_mpi_datatype<T>();
-
+    MPI_Datatype datatype = KokkosComm::Impl::mpi_type_v<T>;
     MPI_Get(origin_addr, count, datatype, source_rank, source_disp, count, datatype, win);
   }
+  // Accumulate
+  template <typename T>
+  void accumulate(const T* origin_addr, int count, int target_rank, 
+                  MPI_Aint target_disp, MPI_Op op) {
+    MPI_Datatype datatype = KokkosComm::Impl::mpi_type_v<T>;
+    MPI_Accumulate(origin_addr, count, datatype,      
+                  target_rank, target_disp,          
+                  count, datatype,                   
+                  op, win);                          
+  }
 
-  /*
-  Function: lock
-  -----------------
-  Description:
-    Locks the window for RMA operations on the specified rank with the given lock type.
-  */
+  // Synchronization Functions
+  // Fence Function:
+  void fence (int assert = 0) { MPI_Win_fence(assert, win); }
+
+  // Lock/Unlock Functions:
   void lock(LockType type, int rank, int assert = 0) {
     int mpi_lock_type = (type == LockType::Exclusive) 
                         ? MPI_LOCK_EXCLUSIVE 
@@ -86,75 +75,45 @@ class Window {
     MPI_Win_lock(mpi_lock_type, rank, assert, win);
   }
 
-  /*
-  Function: unlock
-  -----------------
-  Description:
-    Unlocks the window for RMA operations on the specified rank.
-  */
   void unlock(int rank) { MPI_Win_unlock(rank, win); }
 
   // PSCW Functions:
-  /* 
-  Function: post
-  -----------------
-  Description:
-    Posts an exposure epoch for the window using the specified group of ranks.
-  */
   void post(MPI_Group post_group, int assert = 0) {
       MPI_Win_post(post_group, assert, win);
   }
 
-  /*
-  Function: wait
-  -----------------
-  Description:
-    Waits for the completion of the exposure epoch for the window.
-  */
-  void wait() { MPI_Win_wait(win); }
-
-  /*
-  Function: start
-  -----------------
-  Description:
-    Starts an access epoch for the window using the specified group of ranks.
-  */
   void start(MPI_Group start_group, int assert = 0) {
       MPI_Win_start(start_group, assert, win);
   }
 
-  /*
-  Function: complete
-  -----------------
-  Description:
-    Completes the access epoch for the window.
-  */
   void complete() { MPI_Win_complete(win); }
+
+  void wait() { MPI_Win_wait(win); }
 
  private:
   ViewType v_;
   MPI_Comm comm_;
   MPI_Win win;
 
-  // Helper function to set the datatype for put and get
-  template <typename T>
-  MPI_Datatype get_mpi_datatype() {
-    if (std::is_same<T, int>::value) {
-      return MPI_INT;
-    } else if (std::is_same<T, int64_t>::value) {
-      return MPI_LONG_LONG;
-    } else if (std::is_same<T, float>::value) {
-      return MPI_FLOAT;
-    } else if (std::is_same<T, double>::value) {
-      return MPI_DOUBLE;
-    } else if (std::is_same<T, Kokkos::complex<float>>::value) {
-      return MPI_C_FLOAT_COMPLEX;
-    } else if (std::is_same<T, Kokkos::complex<double>>::value) {
-      return MPI_C_DOUBLE_COMPLEX;
-    } else {
-      return MPI_BYTE;
-    }
-  }
+  // Helper function to set the datatype
+  // template <typename T>
+  // MPI_Datatype get_mpi_datatype() {
+  //   if (std::is_same<T, int>::value) {
+  //     return MPI_INT;
+  //   } else if (std::is_same<T, int64_t>::value) {
+  //     return MPI_LONG_LONG;
+  //   } else if (std::is_same<T, float>::value) {
+  //     return MPI_FLOAT;
+  //   } else if (std::is_same<T, double>::value) {
+  //     return MPI_DOUBLE;
+  //   } else if (std::is_same<T, Kokkos::complex<float>>::value) {
+  //     return MPI_C_FLOAT_COMPLEX;
+  //   } else if (std::is_same<T, Kokkos::complex<double>>::value) {
+  //     return MPI_C_DOUBLE_COMPLEX;
+  //   } else {
+  //     return MPI_BYTE;
+  //   }
+  // }
 };
 
 }  // namespace KokkosComm
