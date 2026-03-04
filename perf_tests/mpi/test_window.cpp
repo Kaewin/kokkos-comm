44e25ad1eb85def9e5d77a58795643aef0165b54
Kaewin <gerbald@protonmail.com>
2026-03-04 08:50:20 -0600

    removed redundant code

--- perf_tests/mpi/test_window.cpp
+++ perf_tests/mpi/test_window.cpp
@@ -362,125 +362,14 @@ void raw_benchmark_pscw_accumulate(benchmark::State &state) {
   state.SetBytesProcessed(sizeof(Scalar) * state.iterations() * n);
 }
 
-template <typename Space, typename View>
-void pscw_get(benchmark::State &, MPI_Comm comm, const Space &, int rank, const View &v,
-              KokkosComm::Window<View> &window, MPI_Group &origin_group, MPI_Group &target_group) {
-  if (rank == 1) {
-    window.post(origin_group);
-  }
-  
-  if (rank == 0) {
-    window.start(target_group);
-    window.get(v.data(), v.size(), 1, 0);  
-    window.complete();
-  }
-  
-  if (rank == 1) {
-    window.wait();
-  }
-}
 
-void benchmark_pscw_get(benchmark::State &state) {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  if (size < 2) {
-    state.SkipWithError("benchmark_pscw_get needs at least 2 ranks");
-    return;
-  }
 
-  auto space = Kokkos::DefaultExecutionSpace();
-  using view_type = Kokkos::View<Scalar *>;
 
-  const int n = state.range(0);
-  view_type v("data", n);
 
-  Kokkos::parallel_for("init", n, KOKKOS_LAMBDA(int i) {
-    v(i) = static_cast<Scalar>(i);
-  });
-  Kokkos::fence();
 
-  KokkosComm::Window<view_type> window(v, MPI_COMM_WORLD);
-  
-  MPI_Group world_group, origin_group, target_group;
-  MPI_Comm_group(MPI_COMM_WORLD, &world_group);
-  int origin_rank = 0;
-  int target_rank = 1;
-  MPI_Group_incl(world_group, 1, &origin_rank, &origin_group);
-  MPI_Group_incl(world_group, 1, &target_rank, &target_group);
 
-  while (state.KeepRunning()) {
-    do_iteration(state, MPI_COMM_WORLD, pscw_get<Kokkos::DefaultExecutionSpace, view_type>,
-                 space, rank, v, std::ref(window), std::ref(origin_group), std::ref(target_group));
-  }
 
-  MPI_Group_free(&origin_group);
-  MPI_Group_free(&target_group);
-  MPI_Group_free(&world_group);
-
-  state.SetBytesProcessed(sizeof(Scalar) * state.iterations() * n);
-}
-
-template <typename Space, typename View>
-void pscw_accumulate(benchmark::State &, MPI_Comm comm, const Space &, int rank, const View &v,
-              KokkosComm::Window<View> &window, MPI_Group &origin_group, MPI_Group &target_group) {
-  if (rank == 1) {
-    window.post(origin_group);
-  }
-  
-  if (rank == 0) {
-    window.start(target_group);
-    window.accumulate(v.data(), v.size(), 1, 0, MPI_SUM);  
-    window.complete();
-  }
-  
-  if (rank == 1) {
-    window.wait();
-  }
-}
-
-void benchmark_pscw_accumulate(benchmark::State &state) {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  if (size < 2) {
-    state.SkipWithError("benchmark_pscw_accumulate needs at least 2 ranks");
-    return;
-  }
-
-  auto space = Kokkos::DefaultExecutionSpace();
-  using view_type = Kokkos::View<Scalar *>;
-
-  const int n = state.range(0);
-  view_type v("data", n);
-
-  Kokkos::parallel_for("init", n, KOKKOS_LAMBDA(int i) {
-    v(i) = static_cast<Scalar>(i);
-  });
-  Kokkos::fence();
-
-  KokkosComm::Window<view_type> window(v, MPI_COMM_WORLD);
-  
-  MPI_Group world_group, origin_group, target_group;
-  MPI_Comm_group(MPI_COMM_WORLD, &world_group);
-  int origin_rank = 0;
-  int target_rank = 1;
-  MPI_Group_incl(world_group, 1, &origin_rank, &origin_group);
-  MPI_Group_incl(world_group, 1, &target_rank, &target_group);
-
-  while (state.KeepRunning()) {
-    do_iteration(state, MPI_COMM_WORLD, pscw_accumulate<Kokkos::DefaultExecutionSpace, view_type>,
-                 space, rank, v, std::ref(window), std::ref(origin_group), std::ref(target_group));
-  }
-
-  MPI_Group_free(&origin_group);
-  MPI_Group_free(&target_group);
-  MPI_Group_free(&world_group);
-
-  state.SetBytesProcessed(sizeof(Scalar) * state.iterations() * n);
-}
 
 
 
